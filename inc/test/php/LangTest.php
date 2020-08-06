3d393e571923f62a32f92bb10f01eceabad821e8
ruan <2369127-ruany@users.noreply.gitlab.com>
2020-08-06 15:45:58 +0200

    [CI] Set up PHPUnit and first unit test

---
+++ inc/test/php/LangTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class LangTest extends TestCase {
+    public function testLanguages(): void {
+        $langs = glob('./lang/*.php');
+        foreach ($langs as $lang) {
+            include_once $lang;
+            $lang_class = $lang;
+            $lang_class = substr($lang_class, 0, strpos($lang_class, ".")); // grab "lang/en_US" from "en_US.utf8.php"
+            $lang_class = substr($lang_class, strlen("lang/")); // grab "en_US" from "lang/en_US"
+
+            $instance = new $lang_class;
+            $this->assertTrue(is_array($instance->array));
+
+            $count = sizeof($instance->array);
+            $this->assertTrue($count > 0);
+
+            echo "Language $lang_class is valid. $count messages defined.";
+        }
+    }
+}
