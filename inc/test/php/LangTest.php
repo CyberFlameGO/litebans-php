c197ff0f492878a2e3de7e9b83b235f338d0dada
ruan <2369127-ruany@users.noreply.gitlab.com>
2020-08-06 16:08:51 +0200

    [CI] Fix LangTest

--- inc/test/php/LangTest.php
+++ inc/test/php/LangTest.php
@@ -4,13 +4,15 @@ use PHPUnit\Framework\TestCase;
 
 final class LangTest extends TestCase {
     public function testLanguages(): void {
+        echo "\n";
+
         $dir = './lang';
         $langs = glob("$dir/*.php");
         foreach ($langs as $lang) {
             include_once $lang;
             $lang_class = $lang;
-            $lang_class = substr($lang_class, 0, strpos($lang_class, ".")); // grab "lang/en_US" from "en_US.utf8.php"
-            $lang_class = substr($lang_class, strlen("$dir/")); // grab "en_US" from "lang/en_US"
+            $lang_class = substr($lang_class, strlen("$dir/")); // grab "en_US.utf8.php" from "./lang/en_US.utf8.php"
+            $lang_class = substr($lang_class, 0, strpos($lang_class, ".")); // grab "en_US" from "en_US.utf8.php"
 
             echo("Testing $lang_class ($lang)...");
             $instance = new $lang_class;
