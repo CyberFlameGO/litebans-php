5b4197eecb3e48bde71e9ae8156a58a77ac242bc
Alexey <yyfwvoh@yandex.ru>
2022-02-23 22:54:32 +0000

    Update ru_RU.utf8.php

--- lang/ru_RU.utf8.php
+++ lang/ru_RU.utf8.php
@@ -1,6 +1,8 @@
 <?php
 // Translated by serflash (Alternative version), The creator of the project SimpleCraft.
 // Перевел serflash (Альтернативая версия), Основатель проекта SimpleCraft.
+// Поправил CXMorgan
+// Corrected by CXMorgan
 
 class ru_RU {
     public function __construct() {
@@ -8,15 +10,15 @@ class ru_RU {
         $array = array();
         $this->array = &$array;
         $array["index.welcome.main"] = "{server} | Бан-лист";
-        $array["index.welcome.sub"] = "Здесь попадают всех нарушители в бан лист";
+        $array["index.welcome.sub"] = "Добро пожаловать";
 
-        $array["title.index"] = 'Домой';
+        $array["title.index"] = 'Главная';
         $array["title.bans"] = 'Баны';
         $array["title.mutes"] = 'Муты';
         $array["title.warnings"] = 'Варны';
         $array["title.kicks"] = 'Кикнутые';
-        $array["title.player-history"] = "Недавние наказания для {name}";
-        $array["title.staff-history"] = "Недавние наказыванных {name}";
+        $array["title.player-history"] = "Недавние наказания игрока {name}";
+        $array["title.staff-history"] = "Недавние наказания администратора {name}";
 
 
         $array["generic.ban"] = "Бан";
@@ -24,10 +26,16 @@ class ru_RU {
         $array["generic.warn"] = "Варн";
         $array["generic.kick"] = "Кик";
 
+        $array["generic.unban"] = "Разбан";
+        $array["generic.unmute"] = "Размут";
+
         $array["generic.banned"] = "Забанен";
-        $array["generic.muted"] = "Закнут";
+        $array["generic.muted"] = "Заткнут";
         $array["generic.warned"] = "Предупрежден";
         $array["generic.kicked"] = "Кикнут";
+
+        $array["generic.unbanned"] = "Разбанен";
+        $array["generic.unmuted"] = "Размучен";
 
         $array["generic.banned.by"] = $array["generic.banned"] . " ";
         $array["generic.muted.by"] = $array["generic.muted"] . " ";
@@ -37,7 +45,7 @@ class ru_RU {
         $array["generic.ipban"] = "IP " . $array["generic.ban"];
         $array["generic.ipmute"] = "IP " . $array["generic.mute"];
 
-        $array["generic.permanent"] = "Постоянный";
+        $array["generic.permanent"] = "Вечный";
         $array["generic.permanent.ban"] = $array['generic.permanent'] . ' ' . $array["generic.ban"];
         $array["generic.permanent.mute"] = $array['generic.permanent'] . ' ' . $array["generic.mute"];
 
@@ -45,35 +53,36 @@ class ru_RU {
         $array["generic.active"] = "Активный";
         $array["generic.inactive"] = "Неактивный";
         $array["generic.expired"] = "Недействителен";
+        $array["generic.expired.kick"] = "Недействителен";
         $array["generic.player-name"] = "Игрок";
 
         $array["page.expired.ban"] = '(Разбанен)';
         $array["page.expired.ban-by"] = '(Разбанил {name})';
-        $array["page.expired.mute"] = '(Снял затычку)';
-        $array["page.expired.mute-by"] = '(Снял затычку {name})';
+        $array["page.expired.mute"] = '(Снял мут)';
+        $array["page.expired.mute-by"] = '(Снял мут {name})';
         $array["page.expired.warning"] = '!' . $array["generic.expired"] . ')';
 
         $array["table.player"] = $array["generic.player-name"];
         $array["table.type"] = $array["generic.type"];
-        $array["table.executor"] = "Привелегий";
+        $array["table.executor"] = "Администратор";
         $array["table.reason"] = "Причина";
         $array["table.date"] = "Дата";
-        $array["table.expires"] = "Истекается";
-        $array["table.received-warning"] = "Получено варн";
+        $array["table.expires"] = "Истекает";
+        $array["table.received-warning"] = "Получено предупреждение";
 
         $array["table.server.name"] = "Сервер";
-        $array["table.server.scope"] = "Область сервера";
-        $array["table.server.origin"] = "Исходной место";
-        $array["table.server.global"] = "*";
+        $array["table.server.scope"] = "Место сервера";
+        $array["table.server.origin"] = "Сервер выдачи";
+        $array["table.server.global"] = "Глобально";
         $array["table.pager.number"] = "Стр.";
 
         $array["action.check"] = "Проверить";
         $array["action.return"] = "Вернутся в {origin}";
 
-        $array["error.missing-args"] = "отсутствует аркументы.";
-        $array["error.name.unseen"] = "{name} небыло еще раньше в игре.";
+        $array["error.missing-args"] = "Отсутствуют аргументы.";
+        $array["error.name.unseen"] = "{name} ранее не играл.";
         $array["error.name.invalid"] = "Неверный игрок.";
-        $array["history.error.uuid.no-result"] = "Небыло нарушители.";
-        $array["info.error.id.no-result"] = "Ошибка: {type} Не найдено в базе данных.";
+        $array["history.error.uuid.no-result"] = "Не имеет нарушений.";
+        $array["info.error.id.no-result"] = "Ошибка: {type} не найден в базе данных.";
     }
 }
